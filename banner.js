9b6dac4de76b2904d007da3b7655fc0c5427b7be
Sharmayu Mirica <67625878+sharmayu@users.noreply.github.com>
2020-06-30 16:53:00 +0800

    uploaded files
    uploaded the files

---
+++ banner.js
@@ -0,0 +1,24 @@
+  var imageURLs = [
+       "repobanner/0.png"
+     , "repobanner/1.png"
+	 , "repobanner/2.png"
+	 , "repobanner/3.png"
+	 , "repobanner/4.png"
+	 , "repobanner/5.png"
+	 , "repobanner/6.png"
+	 , "repobanner/7.png"
+	 , "repobanner/8.png"
+	 , "repobanner/9.png"
+	 , "repobanner/10.png"
+	 , "repobanner/11.png"
+	 , "repobanner/12.png"
+	 , "repobanner/13.png"
+	 
+  ];
+  function getImageTag() {
+    var img = '<img src=\"';
+    var randomIndex = Math.floor(Math.random() * imageURLs.length);
+    img += imageURLs[randomIndex];
+    img += '\" alt=\"Some alt text\"/>';
+    return img;
+  }
