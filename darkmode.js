827c31d3007d4a1028962e0c8500ff281a713669
Sharmayu Mirica <67625878+sharmayu@users.noreply.github.com>
2020-07-02 14:26:58 +0800

    autism

--- darkmode.js
+++ darkmode.js
@@ -25,10 +25,12 @@ function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark'); //add this
+        
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light'); //add this
+        
     }    
 }
 
@@ -39,6 +41,7 @@ if (currentTheme) {
 
     if (currentTheme === 'dark') {
         toggleSwitch.checked = true;
+        
     }
 }
 
