1e3d92993f9f864a89bc2dd1c054579f49a1a8da
Sharmayu <sharmayu@tfwno.gf>
2020-09-01 17:55:51 +0800

    change repobanner by clicking

--- banner.js
+++ banner.js
@@ -1,26 +1,25 @@
-  var imageURLs = [
-       "repobanner/0.png"
-     , "repobanner/1.png"
-	 , "repobanner/2.png"
-	 , "repobanner/3.png"
-	 , "repobanner/4.png"
-	 , "repobanner/5.png"
-	 , "repobanner/6.png"
-	 , "repobanner/7.png"
-	 , "repobanner/8.png"
-	 , "repobanner/9.png"
-	 , "repobanner/10.png"
-	 , "repobanner/11.png"
-	 , "repobanner/12.png"
-	 , "repobanner/13.png"
-	 , "repobanner/14.png"
-	 , "repobanner/15.png"
-	 , "repobanner/16.png"
-  ];
-  function getImageTag() {
-    var img = '<img src=\"';
-    var randomIndex = Math.floor(Math.random() * imageURLs.length);
-    img += imageURLs[randomIndex];
-    img += '\" alt=\"Some alt text\"/>';
-    return img;
-  }
+function pickimg(){
+    var imagenumber = 17 ;
+    var randomnumber = Math.random() ;
+    var rand1 = Math.round( (imagenumber-1) * randomnumber) + 1;
+    images = new Array
+    images[1] = "repobanner/0.png"
+    images[2] = "repobanner/1.png"
+    images[3] = "repobanner/2.png"
+    images[4] = "repobanner/3.png"
+    images[5] = "repobanner/4.png"
+    images[6] = "repobanner/5.png"
+    images[7] = "repobanner/6.png"
+    images[8] = "repobanner/7.png"
+    images[9] = "repobanner/8.png"
+    images[10] = "repobanner/9.png"
+    images[11] = "repobanner/10.png"
+    images[12] = "repobanner/11.png"
+    images[13] = "repobanner/12.png"
+    images[14] = "repobanner/13.png"
+    images[15] = "repobanner/14.png"
+    images[16] = "repobanner/15.png"
+    images[17] = "repobanner/16.png"
+    var image = images[rand1]
+    document.randimg.src = image
+    }
