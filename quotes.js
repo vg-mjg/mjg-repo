ffd8e163933571a6036782e7899cb4005a2434e9
Sharmayu Mirica <67625878+sharmayu@users.noreply.github.com>
2021-05-06 20:21:11 +0800

    New random quotes

---
+++ quotes.js
@@ -0,0 +1,31 @@
+(function newFact() {
+    var facts = [
+    'Do not sleep into that all last', 
+    'UH, ALL THE CAT PEOPLE OR NYAGGERS AS I LIKE TO CALL THEM, GET OFF THIS COMMUNITY BECAUSE ITS FOR CLOSED HANDS ONLY, UNDERSTAND, CAUSE IF YOU DON\'T I WILL MAKE CAT PEOPLE PETS AGAIN!!!', 
+    'No matter how big your hand is, death will end it all.',
+    'Fuck hats.',
+    'did you know that even with 50000 points people lose all the time in mahjong? i thought that was interesting',
+    'Let\'s all watch Shamiko season 2. Together.',
+    'The riichi I took was calculated, but man am I bad at mahjong.',
+    'I won\'t lose as long as I keep winning.',
+    'We can\'t trust any of the suji waits. Throw them all out!',
+    'Even with instinct, courage, and the best game you can play, you will still lose in Mahjong.',
+    'Mahjong causes great damage to the human spirit without a single benefit.',
+    'The sand at the bottom of hell is magical sand.',
+    'Praise meido.',
+    'You can\'t learn mahjong.',
+    '"Do not put the Lord your God to the test" BETAORI',
+    'It\'s just your mind being swept up by temporary random deviations and interpreting them as "flow" or "jinxes."',
+    'I calculate every move with probability, and if it comes to math, I\'m not going to lose to a bunch of otaku playing mahjong because they watched an anime series.',
+    'If there is ever a single shadow of doubt, you will deal in and you will die',
+    'There may be a hundred complex shapes and waits, but you win with just one.',
+    'If my opponent breathes on one, draws on two, and discards on three, then I aim to ron between two and three. Know your opponent’s rhythm and ron when he cannot.',
+    'I do not believe I can tsumo, I know I will.',
+    'The shanshoku, the itsuu, the toitoi. Each offers something the others don\'t. But the tanyao is eternal.'
+
+
+
+    ];
+    var randomFact = Math.floor(Math.random() * facts.length);
+    document.getElementById('factDisplay').innerHTML = facts[randomFact];
+  })();
