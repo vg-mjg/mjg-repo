d0eb3cd457a4ef59633d102b04b2241fa60cb98e
chinponya <czen+github@honk.li>
2023-07-24 16:07:11 +0200

    don't insert html elements for unstyled snowflakes

--- js/index.js
+++ js/index.js
@@ -132,6 +132,22 @@ function SoundAdvice() {
     }
 };
 
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function addSnowflakes() {
+    var limit_flake = 50;
+    setInterval(function() {
+        let dimension = randomInt(3, 9) + "px";
+        var flake = "<div class='drop animate' style='left:" + randomInt(10, window.innerWidth - 20) + "px;width:" + dimension + ";height:" + dimension + "'></div>";
+        $('body').append(flake);
+
+        var list_flake = $('.drop');
+        if (list_flake.length > limit_flake) list_flake[list_flake.length - 1].remove();
+    }, 200);
+}
+
 $(document).ready(function() {
     var updates = "";
     $.getJSON('/updates.json', function(data, textStatus) {
@@ -162,22 +178,8 @@ $(document).ready(function() {
     }
     PickImage();
 
-    $(function() {
-
-        function randomInt(min, max) {
-            return Math.floor(Math.random() * (max - min + 1) + min);
-        };
-
-        var limit_flake = 50;
-        setInterval(function() {
-            let dimension = randomInt(3, 9) + "px";
-            var flake = "<div class='drop animate' style='left:" + randomInt(10, window.innerWidth - 20) + "px;width:" + dimension + ";height:" + dimension + "'></div>";
-            $('body').append(flake);
-
-            var list_flake = $('.drop');
-            if (list_flake.length > limit_flake) list_flake[list_flake.length - 1].remove();
-        }, 200);
-    })
+    // Uncomment this on christmas
+    // addSnowflakes();
 });
 
 $("#ChangeImage").click(function() {
