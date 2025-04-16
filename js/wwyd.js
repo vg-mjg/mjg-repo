38115fea722f6b80d8885e06a738e70c6c4484b0
Sharmayu Mirica <67625878+sharmayu@users.noreply.github.com>
2025-04-16 10:06:08 -0700

    wwyd daily limit removed test

--- js/wwyd.js
+++ js/wwyd.js
@@ -1,162 +1,61 @@
-import { h, Component, render } from "https://esm.sh/preact"
+let puzzles = [];
+let currentIndex = 0;
 
-const wwyds = await fetch("/js/wwyd.json")
-  .then((response) => response.json())
+function displayPuzzle(puzzle, date) {
+  const container = document.getElementById('puzzleContainer');
+  container.innerHTML = '';
 
-function thunk(..._) {
-  return
+  const title = document.createElement('h3');
+  title.textContent = `Puzzle Date: ${date}`;
+  container.appendChild(title);
+
+  const info = document.createElement('p');
+  info.innerHTML = `<b>Seat:</b> ${puzzle.seat}, <b>Round:</b> ${puzzle.round}, <b>Turn:</b> ${puzzle.turn}`;
+  container.appendChild(info);
+
+  const dora = document.createElement('p');
+  dora.innerHTML = `<b>Dora Indicator:</b> ${puzzle.indicator}`;
+  container.appendChild(dora);
+
+  const hand = document.createElement('p');
+  hand.innerHTML = `<b>Hand:</b> ${puzzle.hand.join(' ')}`;
+  container.appendChild(hand);
+
+  const draw = document.createElement('p');
+  draw.innerHTML = `<b>Draw:</b> ${puzzle.draw}`;
+  container.appendChild(draw);
+
+  const answer = document.createElement('p');
+  answer.innerHTML = `<b>Answer:</b> ${puzzle.answer}`;
+  container.appendChild(answer);
+
+  const comment = document.createElement('div');
+  comment.innerHTML = `<b>Comment:</b><br>${puzzle.comment.map(x => Array.isArray(x) ? x.join('') : x).join('')}`;
+  container.appendChild(comment);
 }
 
-function c(...classes) {
-  return classes.filter(e => !!e).join(" ")
+function updatePuzzle() {
+  const dateKeys = Object.keys(puzzles);
+  const key = dateKeys[currentIndex];
+  const puzzle = puzzles[key];
+  displayPuzzle(puzzle, key);
 }
 
-function Tile(props) {
-  const onClick = props.onClick ? () => props.onClick(props.tile) : thunk
-  const selectedTileClass = props.selected ? "tile-selected" : ""
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('wwyd.json')
+    .then(response => response.json())
+    .then(data => {
+      puzzles = data;
+      updatePuzzle();
+    });
 
-  // for some reason the tile will appear on a separate line when it's a div
-  // I don't understand why, so it's a span now
-  return h("span", { title: props.tile, onClick: onClick },
-    h("svg", { class: c("tile", selectedTileClass), fill: "" },
-      h("use", { href: `/others/tiles.svg#${props.tile}` })
-    )
-  )
-}
+  document.getElementById('prevPuzzle').addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + Object.keys(puzzles).length) % Object.keys(puzzles).length;
+    updatePuzzle();
+  });
 
-function TileGroup(props) {
-  return h("div", { class: "tile-group" },
-    props.tiles.map(
-      (tile, index) => h(Tile, {
-        tile: tile, selected: index == props.selectedIndex, onClick: props.onClick
-      }))
-  )
-}
-
-function Hand(props) {
-  let selectedIndex
-  let selectDraw
-  if (props.draw == props.choice) {
-    selectedIndex = -1
-    selectDraw = true
-  } else {
-    selectedIndex = props.tiles.lastIndexOf(props.choice)
-    selectDraw = false
-  }
-  return h("div", { class: "hand" }, [
-    h(TileGroup, { tiles: props.tiles, selectedIndex: selectedIndex, onClick: props.onClick }),
-    h(Tile, { tile: props.draw, selected: selectDraw, onClick: props.onClick })
-  ])
-}
-
-function Comment(props) {
-  if (typeof props.choice != "string") {
-    return ""
-  }
-
-  let comment_blocks = props.comment.map(chunk => {
-    if (typeof chunk == "string") {
-      return h("span", null, chunk)
-    } else if (chunk[0] == "<b>") {
-      return h("strong", null, chunk[1])
-    } else {
-      return h(TileGroup, {tiles: chunk})
-    }
-  })
-
-  return h("span", { class: "wwyd-comment" }, comment_blocks)
-}
-
-function Choice(props) {
-  if (typeof props.choice != "string") {
-    return ""
-  } else if (props.choice == props.answer) {
-    return h("span", { class: "wwyd-answer" }, [
-      h(Tile, { tile: props.choice }),
-      h("p", null, "is correct!")
-    ])
-  } else {
-    return h("span", { class: "wwyd-answer" }, [
-      h(Tile, { tile: props.choice }),
-      h("p", null, "is incorrect. The correct answer is"),
-      h(Tile, { tile: props.answer })
-    ])
-  }
-}
-
-function windName(value) {
-  return {
-    "E": "East",
-    "S": "South",
-    "W": "West",
-    "N": "North",
-  }[value]
-}
-
-function formatDate(date) {
-  const [withoutTime] = date.toISOString().split("T")
-  return withoutTime
-}
-
-function today() {
-  const date = new Date()
-  return formatDate(date)
-}
-
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      choice: localStorage.getItem(`wwyd-${today()}`),
-    }
-    this.setWWYDs()
-  }
-
-  setWWYDs = () => {
-    this.setState({
-      wwyd: wwyds[today()],
-      choice: localStorage.getItem(`wwyd-${today()}`),
-    })
-  }
-
-  chooseDiscard = tile => {
-    localStorage.setItem(`wwyd-${today()}`, tile)
-    this.setState({ choice: tile })
-  }
-
-  render() {
-    if (this.state.wwyd) {
-      return [
-        h("h2", null, "What would you discard?"),
-        h("span", { class: "wwyd-round" }, [
-          h("span", null, [
-            h("p", null, "Round:"),
-            h("p", { class: "wwyd-round-value" }, windName(this.state.wwyd.round))
-          ]),
-          h("span", null, [
-            h("p", null, "Seat:"),
-            h("p", { class: "wwyd-round-value" }, windName(this.state.wwyd.seat))
-          ]),
-          h("span", null, [
-            h("p", null, "Turn:"),
-            h("p", { class: "wwyd-round-value" }, this.state.wwyd.turn)
-          ]),
-          h("span", null, [
-            h("p", null, "Dora indicator"),
-            h(Tile, { tile: this.state.wwyd.indicator })
-          ]),
-        ]),
-        h(Hand, { onClick: this.state.choice ? thunk : this.chooseDiscard, choice: this.state.choice, tiles: this.state.wwyd.hand, draw: this.state.wwyd.draw }),
-        h(Choice, { choice: this.state.choice, answer: this.state.wwyd.answer }),
-        h(Comment, { choice: this.state.choice, comment: this.state.wwyd.comment })
-      ]
-    } else {
-      return [
-        h("h2", null, `There's no WWYD available for ${today()}`),
-      ]
-    }
-  }
-}
-
-let target = document.getElementById("wwyd")
-render(h(App), target)
+  document.getElementById('nextPuzzle').addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % Object.keys(puzzles).length;
+    updatePuzzle();
+  });
+});
