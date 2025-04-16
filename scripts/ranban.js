800e0ae3cdbb18b3c1f80df166375648ad3473ed
Sharmayu Mirica <67625878+sharmayu@users.noreply.github.com>
2025-04-16 01:44:29 -0700

    new banners

---
+++ scripts/ranban.js
@@ -0,0 +1,12 @@
+
+function changeBanner() {
+  var banners = [
+    "banners/0.png",
+    "banners/1.png",
+    "banners/2.jpg",
+    "banners/3.png",
+    "banners/4.png"
+  ];
+  var randomIndex = Math.floor(Math.random() * banners.length);
+  document.getElementById("banner").src = banners[randomIndex];
+}
---
+++ scripts/ranbanmain.js
@@ -0,0 +1,12 @@
+
+function changeBanner() {
+  var banners = [
+    "boards/banners/0.png",
+    "boards/banners/1.png",
+    "boards/banners/2.jpg",
+    "boards/banners/3.png",
+    "boards/banners/4.png"
+  ];
+  var randomIndex = Math.floor(Math.random() * banners.length);
+  document.getElementById("banner").src = banners[randomIndex];
+}
