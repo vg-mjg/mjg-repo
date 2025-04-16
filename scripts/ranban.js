a53eb6132d731ef84ccf705899446b3bb1ed477f
Sharmayu Mirica <67625878+sharmayu@users.noreply.github.com>
2025-04-16 04:39:06 -0700

    one more banner

--- scripts/ranban.js
+++ scripts/ranban.js
@@ -5,7 +5,8 @@ function changeBanner() {
     "banners/1.png",
     "banners/2.jpg",
     "banners/3.png",
-    "banners/4.png"
+    "banners/4.png",
+    "banners/5.png"
   ];
   var randomIndex = Math.floor(Math.random() * banners.length);
   document.getElementById("banner").src = banners[randomIndex];
--- scripts/ranbanmain.js
+++ scripts/ranbanmain.js
@@ -5,7 +5,8 @@ function changeBanner() {
     "boards/banners/1.png",
     "boards/banners/2.jpg",
     "boards/banners/3.png",
-    "boards/banners/4.png"
+    "boards/banners/4.png",
+    "boards/banners/5.png"
   ];
   var randomIndex = Math.floor(Math.random() * banners.length);
   document.getElementById("banner").src = banners[randomIndex];
