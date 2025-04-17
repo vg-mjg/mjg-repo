f34b77fd93a22a1c0f5bf3a1dabcedaf37e1d893
Sharmayu Mirica <67625878+sharmayu@users.noreply.github.com>
2025-04-17 08:58:01 -0700

    added images to the side
    thanks anon

--- scripts/greentext.js
+++ scripts/greentext.js
@@ -39,3 +39,57 @@ window.addEventListener('load', () => {
         observer.observe(hcbBox, { childList: true, subtree: true });
     }
 });
+
+
+function chanTheme() {
+    document.querySelectorAll('[id^="comment_"]').forEach(cmt => {
+      // comment
+      imgLink = cmt.querySelector(":scope > a");
+      text = cmt.querySelector(":scope > blockquote .hcb-comment-body");
+  
+      if (imgLink){
+        // new div for img and post
+        div = document.createElement("div");
+  
+        // sets image size 
+        img = imgLink.querySelector("img");
+        img.style.float = "left";
+        img.style.maxWidth = "120px";
+        img.style.maxHeight = "120px";
+        img.style.marginBottom = "0px";
+        img.style.marginRight = "5px";
+  
+        //click to load
+        const orig = img.src;
+        const high = imgLink.href;
+  
+        imgLink.addEventListener('click', function(e) {
+          e.preventDefault();
+          image = this.querySelector("img");
+  
+          if (image.src === orig) {
+            image.src = high;
+            image.style.opacity = "50%";
+            image.addEventListener('load', function () {
+              image.style.maxWidth = "100%";
+              image.style.maxHeight = "100%";
+              image.style.opacity =  "100%";
+            }, { once: true });
+          } else {
+            image.src = orig;
+            image.style.maxWidth = "120px";
+            image.style.maxHeight = "120px";
+          }
+        });
+  
+        //insert element
+        div.appendChild(imgLink);
+        div.appendChild(text);
+        cmt.querySelector("blockquote").appendChild(div);
+      }
+    });
+  }
+  
+  new MutationObserver(() => {
+    chanTheme()
+  }).observe(document.querySelector("#HCB_comment_box"), { childList: true });
