0b3711ce01cd380f9fa58139ab83f71ce55c9bbe
Ling <florin.dupontel@gmail.com>
2025-05-17 18:31:59 +0200

    Change the emote replacer userscript's default behavior to always work regardless of the image limit

--- others/userscripts/4chan/mjg Emote Replacer.user.js
+++ others/userscripts/4chan/mjg Emote Replacer.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         /mjg/ Emote Replacer
 // @namespace    http://repo.riichi.moe/
-// @version      1.3.4
-// @description  Detects emote strings in imageless posts after the image limit is reached in /mjg/ threads, and displays them as fake images posts.
+// @version      1.3.5
+// @description  Detects emote strings in imageless posts in /mjg/ threads, and displays them as fake images posts.
 // @icon         https://files.catbox.moe/3sh459.png
 // @author       Ling and Anon
 // @match        *://boards.4chan.org/vg/thread/*
@@ -15,7 +15,7 @@
 (function () {
     'use strict';
 
-    const IMAGE_LIMIT = 375; // Change this to 0 if you want the script to work even if the thread hasn't hit the image limit
+    const IMAGE_LIMIT = 0; // Change this to 375 if you want the script to only work after the thread has hit the image limit
     const EMOTE_BASE_URL = 'https://files.riichi.moe/mjg/game%20resources%20and%20tools/Mahjong%20Soul/game%20files/emotes/';
     const EMOTE_REGEX = /\b([a-zA-Z0-9\-\.]+-\d+[cehjk]?t?d?\.(?:png|jpg|jpeg|gif))\b/i;
     const PROCESSED_MARKER = 'data-mjg-emote-processed'; // Values: 'true' (success), 'has-file', 'no-message', 'limit-not-reached', 'emote-not-found', 'checking'
